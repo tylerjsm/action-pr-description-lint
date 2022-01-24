ca43114b1013ca571979f90fcd4e52c10d497439
Tyler Jordan <Tyler.Jordan@it-c02fx4ngmd6r.home.priv>
2022-01-24 23:32:01 +1100

    add template check

--- index.js
+++ index.js
@@ -1,23 +1,49 @@
 const core = require("@actions/core");
 const github = require("@actions/github");
+const fs = require("fs")
 
 function run() {
-  const pullRequest = github.context.payload.pull_request;
+  try{
+    const pullRequest = github.context.payload.pull_request;
+    const templateFile = core.getInput('template-file');
 
-  if(pullRequest) {
-    const body = pullRequest.body;
+    if(pullRequest) {
+      const body = pullRequest.body;
 
-    if (!body) {
-      core.setFailed(
-        "The pull request description is empty. Please add a description."
-      );
-    };
+      if (!body) {
+        core.setFailed(
+          "The pull request description is empty. Please add a description."
+        );
+        return
+      };
 
-    if (body.length >= 7) {
-      core.setFailed(
-        "The pull request description is too short. Please add a description."
-      );
+      if (body.length <= 7) {
+        core.setFailed(
+          "The pull request description is too short. Please add a description."
+        );
+      }
+
+      const template = (() => {
+        try {
+          contents = fs.readFileSync(templateFile, 'utf8')
+          return contents
+
+        } catch (error) {
+          console.log("Unable to locate template file")
+        }
+      })
+
+      if (body == template) {
+        core.setFailed(
+          "The description matches the template for this repository. Please create a unique description."
+        );
+      } else {
+        return
+      }
+
     }
+  } catch (error) {
+    core.setFailed(error.message)
   }
 }
 
